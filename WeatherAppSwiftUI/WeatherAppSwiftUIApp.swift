125050d8d81d74a6182cdebbbe61b6d78e2b0784
Aiden Youkhana <81486691+aidenyoukhana@users.noreply.github.com>
2022-01-14 10:31:46 -0700

    Initial Commit

---
+++ WeatherAppSwiftUI/WeatherAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppSwiftUIApp.swift
+//  WeatherAppSwiftUI
+//
+//  Created by Aiden.Youkhana on 1/14/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
